56ac64206b1daefcb2cd9c9aae9c017b6a3c4a34
Jonatan Villalba <jonatandavidvillalba@gmail.com>
2023-11-01 12:31:00 -0300

    add the footer to the app component

--- mlproject/src/app/app.module.ts
+++ mlproject/src/app/app.module.ts
@@ -6,15 +6,19 @@ import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/header/header.module';
 import { SidebarModule } from './shared/sidebar/sidebar.module';
 import { FormModule } from './shared/form/form.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FooterModule } from './shared/footer/footer.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HeaderModule,
     SidebarModule,
     FormModule,
+    FooterModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
